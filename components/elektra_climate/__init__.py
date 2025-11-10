434a48cdb2e3481b3cfd56b007767e524c53b510
armandiy <95707408+armandiy@users.noreply.github.com>
2025-11-10 02:52:46 +0100

    Update __init__.py

--- components/elektra_climate/__init__.py
+++ components/elektra_climate/__init__.py
@@ -1,14 +1,21 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_NAME
+
+CODEOWNERS = [""]
+
+DEPENDENCIES = ["climate"]
 
 elektra_climate_ns = cg.esphome_ns.namespace("elektra_climate")
 ElektraClimate = elektra_climate_ns.class_("ElektraClimate", climate.Climate, cg.Component)
 
-CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(ElektraClimate),
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(ElektraClimate),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
