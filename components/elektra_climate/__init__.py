57d985dd0dc1e93614f7b54ec62815545184077c
armandiy <95707408+armandiy@users.noreply.github.com>
2025-11-10 02:32:35 +0100

    Update __init__.py

--- components/elektra_climate/__init__.py
+++ components/elektra_climate/__init__.py
@@ -1,28 +1,16 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
-from esphome.const import CONF_NAME, CONF_ID, CONF_PIN
-
-CODEOWNERS = [""]
-
-DEPENDENCIES = ["climate"]
-
-CONF_TX_PIN = "tx_pin"
+from esphome.const import CONF_ID
 
 elektra_climate_ns = cg.esphome_ns.namespace("elektra_climate")
-ElektraClimate = elektra_climate_ns.class_(
-    "ElektraClimate", climate.Climate, cg.Component
-)
+ElektraClimate = elektra_climate_ns.class_("ElektraClimate", climate.Climate, cg.Component)
 
-CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(ElektraClimate),
-        cv.Required(CONF_TX_PIN): cv.int_,
-    }
-).extend(cv.COMPONENT_SCHEMA)
-
+CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(ElektraClimate),
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], config[CONF_TX_PIN])
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
